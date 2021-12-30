618e40dc2f8983a911a9d76e629f0364d63bb230
Angel Fernandez <angel.fernandez@arcelormittal.com>
2021-12-30 10:42:51 +0100

    Relocating test scripts

--- sonar_collector_docker/run_client.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "begin ...."
-
-SONAR_USER=admin SONAR_PASSWORD=admin INFLUX_USER=admin INFLUX_PASSWORD=admin INFLUX_DB=sonarqube_exporter python sonar-client.py
-
-echo "end!!!"
